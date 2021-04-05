9fe1662430b7793d5c5220f241f286c0a9d09639
Dmitry <divelineq@gmail.com>
2021-04-05 11:31:01 +0700

    first commit

---
+++ js/nav.js
@@ -0,0 +1,57 @@
+const img1 = document.querySelector('.img1'),
+    img2 = document.querySelector('.img2'),
+    img3 = document.querySelector('.img3'),
+    img4 = document.querySelector('.img4'),
+    img5 = document.querySelector('.img5'),
+    img6 = document.querySelector('.img6'),
+    img7 = document.querySelector('.img7'),
+    img8 = document.querySelector('.img8'),
+    img9 = document.querySelector('.img9'),
+    img10 = document.querySelector('.img10'),
+    img11 = document.querySelector('.img11'),
+    imgnav = document.querySelector('[src="img/nav/micro.jpg"]');
+
+
+img1.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/micro.jpg')
+})
+
+img2.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/micro_se.jpg')
+})
+
+img3.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/adiva.jpg')
+})
+
+img4.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/adiva_se.jpg')
+})
+
+img5.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/micro_droplet.jpg')
+})
+
+img6.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/micro_se_droplet.jpg')
+})
+
+img7.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/adiva_droplet.jpg')
+})
+
+img8.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/adiva_se_droplet.jpg')
+})
+
+img9.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/strada_2.jpg')
+})
+
+img10.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/subwoofers.jpg')
+})
+
+img11.addEventListener('mouseenter', () => {
+    imgnav.setAttribute('src', 'img/nav/accessories.jpg')
+})
---
+++ js/sliders.js
@@ -0,0 +1,27 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1500,
+});
+
+$('.slider2').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1500,
+});
+
+
+
+
+
+
